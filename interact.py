ce68e7f0a51f725aeee746109cd5c8c516e9a368
ChristianYang37 <93643049+ChristianYang37@users.noreply.github.com>
2023-03-30 15:33:13 +0800

    Update interact.py

--- interact.py
+++ interact.py
@@ -1,4 +1,5 @@
 # https://sysu-ifa.streamlit.app/
+import os
 import streamlit as st
 import paramiko
 import time
@@ -21,8 +22,9 @@ class SSH:
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.client.connect(hostname=hostname, port=port, username=username, password=password)
+
         self.file = './%s.pkl' % random_filename()
-        self.target_file = './ChatGLM-6B' + self.file[1:]
+        self.target_file = './ChatGLM-6B/tmp' + self.file[1:]
 
     def post(self, input_text, history):
         transport = self.client.get_transport()
@@ -33,20 +35,20 @@ class SSH:
         ssh.invoke_shell()
 
         ssh.send(bytes("cd ChatGLM-6B\n", encoding='utf-8'))
-        time.sleep(10)
+        time.sleep(3)
         _ = ssh.recv(8192)
 
         param = {'input_text': input_text, 'history': history}
         self.put(param, transport)
 
-        cmd = f"python post.py --file_name %s\n" % self.file
+        cmd = f"python post.py --file_name ./tmp/%s\n" % self.file[2:]
         ssh.send(bytes(cmd, encoding='utf-8'))
-        time.sleep(10)
-        response = ssh.recv(8192)
+        time.sleep(20)
+        response = ssh.recv(8192).decode('utf-8')
 
-        print(response.decode('utf-8'))
+        print(response)
 
-        response = eval(response.decode('utf-8').split('\r')[1][1:])
+        response = eval(response.split('\r')[1][1:])
         response, history = response['response'], response['history']
 
         return response, history
@@ -56,24 +58,10 @@ class SSH:
             pickle.dump(param, file, True)
         sftp = paramiko.SFTPClient.from_transport(transport)
         sftp.put(self.file, self.target_file)
-
-    def __del__(self):
-        pass
-        # transport = self.client.get_transport()
-        #
-        # ssh = transport.open_session()
-        #
-        # ssh.get_pty()
-        # ssh.invoke_shell()
-        #
-        # ssh.send(bytes("rm -rf %s\n" % self.target_file, encoding='utf-8'))
-        # os.system("rm -rf %s\n" % self.file)
-        # self.client.close()
+        os.remove(self.file)
 
 
 class APP:
-    txt = ''
-
     def __init__(self):
         self.client = SSH(st.secrets['hostname'], st.secrets['password'], st.secrets['username'], st.secrets['port'])
         # st.title('iFA: 你的智能法律咨询顾问')
